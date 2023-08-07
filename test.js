da66fe6325bdb765b7e3af5723eba7dfecaf1c11
Saliha İptes <salihaipts@gmail.com>
2023-08-07 17:30:22 +0300

    add_requestmethods

---
+++ test.js
@@ -0,0 +1,10 @@
+const a = 5;
+const b = 10;
+
+const sum = () => {
+    const result = a + b;
+    return result;
+}
+const c = sum();
+
+console.log(c);
--- todo.js
+++ todo.js
@@ -1,9 +1,69 @@
+const bodyParser = require('body-parser')
 // import dependencies
-const express = require("express")
+const express = require("express");
 // Create a new express application object
-const app = express()
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
+const todos = [];
+
+app.listen(4444, () => {
+    console.log("Server is Listening on port 4444");
+});
+
+app.get('/', (req, res)=>{
+    console.log(req);
+    res.send(todos);
+});
+
+app.post('/', (req, res) =>{
+    const data = req.body;
+
+    if(todos.length == 0){
+        todos.push({id:1, name:data.name, description:data.description});
+    }else{
+        const lastElement = todos[todos.length-1];
+        const lastId = lastElement.id;
+        todos.push({id: lastId+1, name:data.name, description:data.description});
+    }
+
+    res.send(todos);
+});
+
+app.put('/', (req, res) =>{
+    const data = req.body;
+    
+    for(let i=0; i<todos.length; i++){
+        let todoItem = todos[i];
+        if(todoItem.id == req.query.id){
+            todoItem['name'] = data.name;
+            todoItem["description"] = data.description;
+        }
+    }
+    res.send(todos);
+    //console.log(req.query);
+    //console.log(req.body);
+});
+
+app.delete('/', (req, res) =>{
+
+    
+    for(let i=0; i<todos.length; i++){
+        let todoItem = todos[i];
+        if(todoItem.id == req.query.id){
+            todos.splice(i);
+        }
+    }
+    
+    res.send(todos);
+    //console.log(req.query.id);
+});
+
 // Array for Todos
-const todos = []
+/*const todos = []
 //middleware
 app.use("/static", express.static("static"))
 app.use(express.urlencoded({extended: true}))
@@ -26,4 +86,4 @@ app.post("/", (req, res) => {
 // run your application, so it listens on port 4444
 app.listen(4444, () => {
     console.log("Server is Listening on port 4444")
-})
+})*/
